7c4f29a136bba740f4cfaf07515c73631fe88154
miv <cu_bot@naver.com>
2024-02-07 16:13:12 +0900

    switch -> when

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,14 +1,17 @@
 package main.kotlin
 
 import main.kotlin.Color.*
+import java.time.Duration
+import java.time.LocalDateTime
 
 enum class Color {
     RED, ORANGE, YELLOW, GREEN, BLUE
 }
 
 fun main() {
-    val mix = mix(YELLOW, BLUE)
-    println(mix)
+    printObject("OBJECT")
+    printObject(Duration.ofNanos(3456))
+    printObject(LocalDateTime.of(2023, 12, 12, 10, 10))
 }
 
 fun getKoreanColorName(color: Color): String = when (color) {
@@ -17,6 +20,14 @@ fun getKoreanColorName(color: Color): String = when (color) {
     YELLOW -> "노랑"
     GREEN -> "녹색"
     BLUE -> "파랑"
+}
+
+// Unit 이 함수는 응답값이 없다. 자바에서 void와 같다.
+fun printObject(obj: Any): Unit = when (obj) {
+    is String -> println(obj.lowercase())
+    is Duration -> println(obj.nano)
+    is LocalDateTime -> println(obj.month)
+    else -> println("Unknown type")
 }
 
 fun mix(c1: Color, c2: Color) =
